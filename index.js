90ccea8435143587d169e5136589ea2380ecf339
Wumi Afo <117525566+babycodeher@users.noreply.github.com>
2023-02-26 18:13:36 +0000

    added css file

--- index.js
+++ index.js
@@ -18,78 +18,75 @@ const Engineer = require("./lib/Engineer");
 
 let team = [];
 
-startProgram()
-
 async function startProgram() {
-    let { data } = await inquirer
-    .prompt([
-        {
-            type: 'list',
-            name: 'employee',
-            message: 'Which team member will you like to add?',
-            choices: ['Manager', 'Engineer', 'Intern'],
-        },
-        {
-            type: 'input',
-            name: 'name',
-            message: "What's the employee's name?",
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: "What's the employee's id number",
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: "What is the employee's email address?",
-        },
-        {
-            type: 'input',
-            name: 'officeNumber',
-            message: "What is your office number?",
-            when(answers) {
-                return answers.employee === "Manager";
+    let data = await inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "employee",
+                message: "Which team member will you like to add?",
+                choices: ["Manager", "Engineer", "Intern"],
             },
-        },
-        {
-            type: 'input',
-            name: 'gitHub',
-            message: "What is your gitHub username?",
-            when(answers) {
-                return answers.employee === "Engineer";
+            {
+                type: "input",
+                name: "name",
+                message: "What's the employee's name?",
             },
-        },
-        {
-            type: 'input',
-            name: 'school',
-            message: "What school do you attend?",
-            when(answers) {
-                return answers.employee === "Intern";
+            {
+                type: "input",
+                name: "id",
+                message: "What's the employee's id number",
             },
-        },
-        
-        // {
-        //     type: 'input',
-        //     name: 'gitHub',
-        //     message: "What's your GitHub username?",
-        // },
-        // {
-        //     type: 'input',
-        //     name: 'email',
-        //     message: "What's your email address?",
-        // },
-    ])
-    const manager1 = (new Manager(data.name, data.id, data.email, data.officeNumber));
-    // const manager = (new Manager(input.name, input.id, input.email, input.officeNumber))
-    // const engineer = (new Engineer(input.name, input.id, input.email, input.gitHub))
-    // const intern = (new Intern(input.name, input.id, input.email, input.school))
-    
-    team.push(manager);
-    // team.push(engineer)
-    // team.push(intern)
+            {
+                type: "input",
+                name: "email",
+                message: "What is the employee's email address?",
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "What is your office number?",
+                when(answers) {
+                    return answers.employee === "Manager";
+                },
+            },
+            {
+                type: "input",
+                name: "gitHub",
+                message: "What is your gitHub username?",
+                when(answers) {
+                    return answers.employee === "Engineer";
+                },
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "What school do you attend?",
+                when(answers) {
+                    return answers.employee === "Intern";
+                },
+            },
+            {
+                type: "confirm",
+                name: "askAgain",
+                message: "Want to enter another employee's detail? (just hit enter for YES)?",
+                default: true,
+            },
+        ]);
+
+    const manager1 = new Manager(data.name, data.id, data.email, data.officeNumber);
+    const engineer1 = new Engineer(data.name, data.id, data.email, data.gitHub);
+    const intern1 = new Intern(data.name, data.id, data.email, data.school);
+    // const manager = (new Manager(input.name, input.id, input.email, input.officeNumber));
+
+    team.push(manager1);
+    team.push(engineer1);
+    team.push(intern1);
+        // team.push();
 
     let htmlDoc = render(team)
 
     await fs.writeFile(outputPath, htmlDoc);
 }
+
+startProgram();
